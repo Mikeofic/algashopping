447ddb3ec79a08f81a3dd2fa8703f3c4fc601e4b
Jean <jean.michel@b2wdigital.com>
2022-09-27 00:10:29 -0300

    added checkbox shared component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react'
+import Checkbox from '../../shared/Checkbox'
 import AppContainer from '../AppContainer'
 import Header from '../Header'
 import { Wrapper, Container } from './App.styles'
 
 function App () {
+  const [lettuce, setLettuce] = useState(false)
+
   return (
     <Wrapper>
       <Container>
         <Header/>
         <AppContainer  
-          left={<div>Produtos disponívels</div>}
+          left={
+            <div>Produtos disponívels: 
+              <Checkbox title="Alface" value={lettuce} onClick={() => {setLettuce(!lettuce)}}/>
+            </div>}
           middle={<div>Sua lista de compras</div>}
           right={<div>Estatísticas</div>}
         />
